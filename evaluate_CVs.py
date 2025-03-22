58cc654888210d56599d80594ed14cb0eb45e33e
kapilw25 <kapilw25@gmail.com>
2025-03-22 10:03:50 -0700

    2 CV models generating images from single script

--- evaluate_CVs.py
+++ evaluate_CVs.py
@@ -2,6 +2,7 @@ import torch
 from diffusers import StableDiffusionPipeline, AutoPipelineForText2Image
 import os
 from evaluation import EvaluationMetrics
+from PIL import Image
 
 # CLear GPU cache to free memory
 torch.cuda.empty_cache()
@@ -10,49 +11,32 @@ torch.cuda.empty_cache()
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 # Ensure the 'models' and 'image_generated' directories exist
-os.makedirs("models", exist_ok=True)
 os.makedirs("image_generated", exist_ok=True)
 
-# Customer cache path for downloaded models
-os.environ["HF_HOME"] = os.path.abspath("models")
+# remove manual model caching logic and rely on HF default cache directory
+if "HF_HOME" in os.environ:
+    del os.environ["HF_HOME"]
 
 # shared prompt
 prompts = ["A cat holding a sign that says hello world",
            "A futuristic city with flying cars",
            "An astronaut riding a horse in space"
            ]
-# ------------------------- Model Paths for Local Loading -------------------------
-model_sd_v1_5_path = "models/stable-diffusion-v1-5"
-model_sdxl_turbo_path = "models/sdxl-turbo"
-
-# ------------------------- Download or load from Local -------------------------
-# Helper Function to Handle Partial Downloads
-def load_model(pipe_class, model_name, model_path, **kwargs):
-    try:
-        
 
 # -----CV model1: Stable Diffusion v1.5---------------------------------------
-if not os.path.exists(model_sd_v1_5_path):
-    pipe_sd_v1_5 = StableDiffusionPipeline.from_pretrained(
-        "sd-legacy/stable-diffusion-v1-5",
-        torch_dtype= torch.float16,
-        cache_dir = model_sd_v1_5_path
-    )
-else:
-    pipe_sd_v1_5 = StableDiffusionPipeline.from_pretrained(model_sd_v1_5_path)
+pipe_sd_v1_5 = StableDiffusionPipeline.from_pretrained(
+    "sd-legacy/stable-diffusion-v1-5",
+    torch_dtype= torch.float16
+)
     
 pipe_sd_v1_5.enable_model_cpu_offload()   # Automatically offloads to CPU/GPU as needed
 
 # ----- CV Model2: SDXL Turbo ---------------------------------------
-if not os.path.exists(model_sdxl_turbo_path):
-    pipe_sdxl_turbo = AutoPipelineForText2Image.from_pretrained(
-        "stabilityai/sdxl-turbo",
-        torch_dtype=torch.float16,
-        variant="fp16",
-        cache_dir = model_sdxl_turbo_path
-    )
-else:
-    pipe_sdxl_turbo = AutoPipelineForText2Image.from_pretrained(model_sdxl_turbo_path)
+pipe_sdxl_turbo = AutoPipelineForText2Image.from_pretrained(
+    "stabilityai/sdxl-turbo",
+    torch_dtype=torch.float16,
+    variant="fp16",
+)
 
 pipe_sdxl_turbo.enable_model_cpu_offload()
 # ------------------------- Batch Image Generation -------------------------
@@ -70,21 +54,21 @@ for model_name, pipe in models.items():
         images.setdefault(model_name, []).append(image_path)
         
 # ------------------------- Evaluation -------------------------
-for model_name, img_paths in images.items():
-    generated_images = [Image.open(img) for img  in img_paths]
+# for model_name, img_paths in images.items():
+#     generated_images = [Image.open(img) for img  in img_paths]
     
-    # Evaluation Metrics
-    clip_scores = EvaluationMetrics.calculate_clip_score(generated_images, prompts[0])
-    bleu_scores = EvaluationMetrics.calculate_bleu_score(prompts[0], prompts)
-    cosine_scores = EvaluationMetrics.calculate_cosine_similarity(generated_images, prompts[0])
-    inference_time = EvaluationMetrics.measure_inference_time(models[model_name], prompts)
-    gpu_memory = EvaluationMetrics.measure_gpu_memory()
-    throughput = EvaluationMetrics.measure_throughput(inference_time * len(prompts), len(prompts))
+#     # Evaluation Metrics
+#     clip_scores = EvaluationMetrics.calculate_clip_score(generated_images, prompts[0])
+#     bleu_scores = EvaluationMetrics.calculate_bleu_score(prompts[0], prompts)
+#     cosine_scores = EvaluationMetrics.calculate_cosine_similarity(generated_images, prompts[0])
+#     inference_time = EvaluationMetrics.measure_inference_time(models[model_name], prompts)
+#     gpu_memory = EvaluationMetrics.measure_gpu_memory()
+#     throughput = EvaluationMetrics.measure_throughput(inference_time * len(prompts), len(prompts))
     
-    print(f"{model_name} Metrics:")
-    print(f"CLIP scores: {clip_scores}")
-    print(f"BLEU scores: {bleu_scores}")
-    print(f" Cosine similarity Scores: {cosine_scores}")
-    print(f"GPU Memory Usage: {gpu_memory:.2f} MB")
-    print(f"Inference Time: {inference_time:.2f} sec/image")
-    print(f" Throughput: {throughput:.2f} img/sec")
+#     print(f"{model_name} Metrics:")
+#     print(f"CLIP scores: {clip_scores}")
+#     print(f"BLEU scores: {bleu_scores}")
+#     print(f" Cosine similarity Scores: {cosine_scores}")
+#     print(f"GPU Memory Usage: {gpu_memory:.2f} MB")
+#     print(f"Inference Time: {inference_time:.2f} sec/image")
+#     print(f" Throughput: {throughput:.2f} img/sec")
--- evaluation.py
+++ evaluation.py
@@ -7,43 +7,12 @@ from torchvision import transforms
 from PIL import Image
 import numpy as np
 from sentence_transformers import SentenceTransformer, util
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
-# Ensure 'models/' directory exists for local caching
-os.makedirs("models", exist_ok=True)
-# Customer cache path for downloaded models
-os.environ["HF_HOME"] = os.path.abspath("models")
-
-# ================================
-# Download or Load from Local Directory
-# ================================
-clip_model_path = "models/clip-vit-base-patch32"
-clip_processor_path = "models/clip-vit-base-patch32-processor"
-sbert_model_path = "models/all-MiniLM-L6-v2"
-
-# Load CLIP model
-if not os.path.exists(clip_model_path):
-    clip_model = CLIPModel.from_pretrained(
-        "openai/clip-vit-base-patch32",
-        cache_dir=clip_model_path)
-else:
-    clip_model = CLIPModel.from_pretrained(clip_model_path)
-    
-# Load CLIP Processor
-if not os.path.exists(clip_processor_path):
-    clip_processor = CLIPProcessor.from_pretrained(
-        "openai/clip-vit-base-patch32",
-        cache_dir=clip_processor_path)
-else:
-    clip_model = CLIPModel.from_pretrained(clip_processor_path)
-    
-# Load SentenceTransformer Model
-if not os.path.exists(sbert_model_path):
-    sbert_model = SentenceTransformer(
-        'sentence-transformers/all-MiniLM-L6-v2',
-        cache_folder=sbert_model_path)
-else:
-    sbert_model = CLIPModel.from_pretrained(sbert_model_path)
+# Load models 
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32",)
+sbert_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 
 # ================================
@@ -68,12 +37,15 @@ class EvaluationMetrics:
     def calculate_bleu_score(reference_text, generated_texts):
         """Batch Calculation for BLEU score"""
         reference = [reference_text.lower().split()]
+        smooth_fn = SmoothingFunction().method1
         return [
-            sentence_bleu(reference, text.lower().split()) \
+            sentence_bleu(reference,
+                        text.lower().split(),
+                        smoothing_function = smooth_fn)
                 for text in generated_texts]
         
     @staticmethod
-    def calculate_cosine_similarity(image, prompt):
+    def calculate_cosine_similarity(images, prompt):
         """Batch Calculation for cosine similarity"""
         prompt_embedding = sbert_model.encode(prompt)
         return [
